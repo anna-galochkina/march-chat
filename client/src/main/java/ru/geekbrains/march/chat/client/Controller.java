37dbe32fa34551eb35957b34628c1d421d41384d
kr0085@mail.ru <TzrV1f00sxK3e>
2021-03-15 15:23:21 +0400

    lesson2_7

--- client/src/main/java/ru/geekbrains/march/chat/client/Controller.java
+++ client/src/main/java/ru/geekbrains/march/chat/client/Controller.java
@@ -1,61 +1,101 @@
 package ru.geekbrains.march.chat.client;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.URL;
-import java.util.ResourceBundle;
 
-import ru.geekbrains.march.chat.server.ServerApp;
-
-public class Controller implements Initializable {
-
+public class Controller {
     @FXML
-    TextField msgField;
+    TextField msgField, usernameField;
 
     @FXML
     TextArea msgArea;
 
+    @FXML
+    HBox loginPanel, msgPanel;
+
     private Socket socket;
     private DataInputStream in;
     private DataOutputStream out;
+    private String username;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void setUsername(String username) {
+        this.username = username;
+        if (username != null) {
+            loginPanel.setVisible(false);
+            loginPanel.setManaged(false);
+            msgPanel.setVisible(true);
+            msgPanel.setManaged(true);
+        } else {
+            loginPanel.setVisible(true);
+            loginPanel.setManaged(true);
+            msgPanel.setVisible(false);
+            msgPanel.setManaged(false);
+        }
+    }
+
+    public void login() {
+        if (socket == null || socket.isClosed()) {
+            connect();
+        }
+
+        if (usernameField.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Имя пользователя не может быть пустым", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
+        try {
+            out.writeUTF("/login " + usernameField.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connect() {
         try {
             socket = new Socket("localhost", 8189);
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
             Thread t = new Thread(() -> {
-                int msgCount = 0;
-
                 try {
+                    // Цикл авторизации
                     while (true) {
                         String msg = in.readUTF();
-                        if (msg.length() > 0) {
-                            if (msg.equals(ServerApp.COMMAND_STAT)) {
-                                msg = "Количество сообщений - " + msgCount;
-                            } else {
-                                msgCount++;
-                            }
-                            msgArea.appendText(msg + "\n");
+                        if (msg.startsWith("/login_ok ")) {
+                            setUsername(msg.split("\\s")[1]);
+                            break;
                         }
+                        if (msg.startsWith("/login_failed ")) {
+                            String cause = msg.split("\\s", 2)[1];
+                            msgArea.appendText(cause + "\n");
+                        }
+                    }
+                    // Цикл общения
+                    while (true) {
+                        String msg = in.readUTF();
+                        msgArea.appendText(msg + "\n");
                     }
                 } catch (IOException e) {
                     e.printStackTrace();
+                } finally {
+                    disconnect();
                 }
             });
             t.start();
         } catch (IOException e) {
-            throw new RuntimeException("Unable to connect to server [ localhost:8189 ]");
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Невозможно подключиться к серверу", ButtonType.OK);
+            alert.showAndWait();
         }
     }
 
@@ -63,9 +103,21 @@ public class Controller implements Initializable {
         try {
             out.writeUTF(msgField.getText());
             msgField.clear();
+            msgField.requestFocus();
         } catch (IOException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Невозможно отправить сообщение", ButtonType.OK);
             alert.showAndWait();
+        }
+    }
+
+    private void disconnect() {
+        setUsername(null);
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- server/src/main/java/ru/geekbrains/march/chat/server/ServerApp.java
+++ server/src/main/java/ru/geekbrains/march/chat/server/ServerApp.java
@@ -1,66 +1,7 @@
 package ru.geekbrains.march.chat.server;
 
-import java.io.Closeable;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class ServerApp {
-
-    public static final String COMMAND_STAT = "/stat";
-
-    // Домашнее задание:
-    // 1. Разберитесь с кодом, все вопросы можно писать в комментариях к дз
-    // 2. Пусть сервер подсчитывает количество сообщений от клиента
-    // 3. Если клиент отправит команду '/stat', то сервер должен выслать клиенту
-    // не эхо, а сообщение вида 'Количество сообщений - n'
-
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(8189)) {
-            System.out.println("Сервер запущен на порту 8189. Ожидаем подключение клиента...");
-            Socket socket = serverSocket.accept();
-            DataInputStream in = new DataInputStream(socket.getInputStream());
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            System.out.println("Клиент подключился");
-
-//            п3 если нужно общее количеcтво сообщений без учета подсчета по каждому отдельному клиенту, то код ниже
-//
-//            int msgCount = 0;
-//            while (true) {
-//                String msg = in.readUTF().trim();
-//                if (msg.length() > 0) {
-//                    if (!msg.toLowerCase().equals(COMMAND_STAT)) {
-//                        msg = "ECHO: " + msg;
-//                        msgCount++;
-//                    } else {
-//                        msg = "Количество сообщений - " + msgCount;
-//                    }
-//                    out.writeUTF(msg);
-//                    System.out.println(msg);
-//                }
-//            }
-
-
-//            иначе этот + фикс в контроллере, чтобы считать сообщения у каждого клиента по отдельности (но все равно как-то криво)
-//            пришлось пробрасывать значение константы клиенту, чтобы он правильно подсчитывал сообщения (сообщения о запросе статистики не должны считаться),
-//            иначе если отправлять сообщение тут, то клиент получает сообщение о количестве от сервера и воспринимает его как свое и увеличивает счетчик сообщений
-//            пробовала тут создать статическую переменную msgCount, увеличивать ее, когда сервер отправляет эхо и уже на клиенте брать ее значение
-//            (чтобы можно было по разным командам подчитывать сообщения всего /total_stat и от каждого клиента /stat), но на клиенте в этой переменной,
-//            всегда по какой то причине 0. 
-
-//
-            while (true) {
-                String msg = in.readUTF().trim();
-                if (msg.length() > 0) {
-                    msg = !msg.toLowerCase().equals(COMMAND_STAT) ? "ECHO: " + msg : COMMAND_STAT;
-                    out.writeUTF(msg);
-                    System.out.println(msg);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        new Server(8189);
     }
 }
